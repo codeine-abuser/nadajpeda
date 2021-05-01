045a80865818dfc27c5d87150c73022b657c9cd1
damo <72207614+damo-scripts@users.noreply.github.com>
2021-05-01 13:39:29 +0200

    Add files via upload

---
+++ server.lua.lua
@@ -0,0 +1,13 @@
+ESX = nil
+local Webhook = 'webhooklink'
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand('nadajpeda', function(source, args)
+	id = args[1]
+	ped = args[2]
+	if ped == 'muscle' then
+		TriggerClientEvent('dzieciak', id)
+		PerformHttpRequest(Webhook, function(Error, Content, Head) end, 'POST', json.encode({username = 'damo', content = '**pedy**: `ID:` **'..source..'** *nadal peda dla* `ID:` **'..id..'**\n```ped: dzieciak ```'}), { ['Content-Type'] = 'application/json' })			
+	end
+	end)
