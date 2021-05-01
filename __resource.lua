147abc960c2d5c5a069e2f1f65bf06d3cd25124a
damo <72207614+damo-scripts@users.noreply.github.com>
2021-05-01 13:45:18 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,9 +3,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 author 'damo#3333'
 
 server_scripts {
-  'server/main.lua'
+  'server.lua'
 }
 
 client_scripts {
-  'client/main.lua'
+  'client.lua'
 }
