2f544464324ad00d7fc41032dfc2a35bd4755027
damo <72207614+damo-scripts@users.noreply.github.com>
2021-05-01 13:44:59 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,7 +6,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterCommand('nadajpeda', function(source, args)
 	id = args[1]
 	ped = args[2]
-	if ped == 'muscle' then
+	if ped == 'dzieciak' then
 		TriggerClientEvent('dzieciak', id)
 		PerformHttpRequest(Webhook, function(Error, Content, Head) end, 'POST', json.encode({username = 'damo', content = '**pedy**: `ID:` **'..source..'** *nadal peda dla* `ID:` **'..id..'**\n```ped: dzieciak ```'}), { ['Content-Type'] = 'application/json' })			
 	end
